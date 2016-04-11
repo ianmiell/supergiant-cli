68d9f27eafc6177c6c4a311c9a79fa211ed19109
spankenstein <spankenstien@gmail.com>
2016-04-11 17:20:55 -0500

    Repo path push edit.

--- hack/ghr.sh
+++ hack/ghr.sh
@@ -2,11 +2,11 @@
 VERSION=$(tail -1 version.go | awk '{print $5}' | sed 's/"//g')
 if [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then
   echo "Releasing supergiant-cli version: ${VERSION}, pre-release"
-  ghr --username spankenstein --token $GITHUB_TOKEN --replace --prerelease --debug $VERSION dist/
+  ghr --username supergiant --token $GITHUB_TOKEN --replace --prerelease --debug $VERSION dist/
   exit 0
 elif [ ! -z "$TRAVIS_TAG" ]; then
   echo "Releasing supergiant-cli version: ${TRAVIS_TAG}, as latest release."
-  ghr --username spankenstein --token $GITHUB_TOKEN --replace --debug $TRAVIS_TAG dist/
+  ghr --username supergiant --token $GITHUB_TOKEN --replace --debug $TRAVIS_TAG dist/
   exit 0
 fi
 echo "Unable to determine tag."
