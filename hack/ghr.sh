1781b036bf59aba4dc129f2e41d5c2d6713a9201
spankenstein <spankenstien@gmail.com>
2016-04-11 16:55:36 -0500

    Fixed a syntax issue with the bin tagging.

--- hack/ghr.sh
+++ hack/ghr.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 VERSION=$(tail -1 version.go | awk '{print $5}' | sed 's/"//g')
-if [ -z "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then
   echo "Releasing supergiant-cli version: ${VERSION}, pre-release"
   ghr --username spankenstein --token $GITHUB_TOKEN --replace --prerelease --debug $VERSION dist/
-else
+  exit 0
+elif [ ! -z "$TRAVIS_TAG" ]; then
   echo "Releasing supergiant-cli version: ${TRAVIS_TAG}, as latest release."
   ghr --username spankenstein --token $GITHUB_TOKEN --replace --debug $TRAVIS_TAG dist/
+  exit 0
 fi
+echo "Unable to determine tag."
+exit 5
