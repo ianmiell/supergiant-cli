80d13abe9bc7075689264f5c9b271abf8280048c
spankenstein <spankenstien@gmail.com>
2016-04-11 16:11:54 -0500

    Added ghr logic for pre-release versions verses official release versions.

---
+++ hack/ghr.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VERSION=$(tail -1 version.go | awk '{print $5}' | sed 's/"//g')
+if [ -z "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Releasing supergiant-cli version: ${VERSION}, pre-release"
+  ghr --username spankenstein --token $GITHUB_TOKEN --replace --prerelease --debug version dist/
+else
+  echo "Releasing supergiant-cli version: ${TRAVIS_TAG}, as latest release."
+  ghr --username spankenstein --token $GITHUB_TOKEN --replace --debug $TRAVIS_TAG dist/
+fi
