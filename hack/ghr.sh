f46e3c7b407514bcb42c80b19cb257e094529007
spankenstein <spankenstien@gmail.com>
2016-04-11 16:13:25 -0500

    Added version flag for pre-releases in ghr.

--- hack/ghr.sh
+++ hack/ghr.sh
@@ -2,7 +2,7 @@
 VERSION=$(tail -1 version.go | awk '{print $5}' | sed 's/"//g')
 if [ -z "$TRAVIS_TAG" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Releasing supergiant-cli version: ${VERSION}, pre-release"
-  ghr --username spankenstein --token $GITHUB_TOKEN --replace --prerelease --debug version dist/
+  ghr --username spankenstein --token $GITHUB_TOKEN --replace --prerelease --debug $VERSION dist/
 else
   echo "Releasing supergiant-cli version: ${TRAVIS_TAG}, as latest release."
   ghr --username spankenstein --token $GITHUB_TOKEN --replace --debug $TRAVIS_TAG dist/
