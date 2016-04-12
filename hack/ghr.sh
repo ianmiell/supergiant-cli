682194674def541643ce81765af2bb2447f1a6a7
spankenstein <spankenstien@gmail.com>
2016-04-12 14:22:47 -0500

    Cross compile was duplicating release versions. Now all pre-release master builds will be tagged unstable.

--- hack/ghr.sh
+++ hack/ghr.sh
@@ -2,7 +2,7 @@
 VERSION=$(tail -1 version.go | awk '{print $5}' | sed 's/"//g')
 if [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then
   echo "Releasing supergiant-cli version: ${VERSION}, pre-release"
-  ghr --username supergiant --token $GITHUB_TOKEN --replace --prerelease --debug $VERSION dist/
+  ghr --username supergiant --token $GITHUB_TOKEN --replace --prerelease --debug unstable-$VERSION dist/
   exit 0
 elif [ ! -z "$TRAVIS_TAG" ]; then
   echo "Releasing supergiant-cli version: ${TRAVIS_TAG}, as latest release."
