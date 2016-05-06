7852ef7b63465da0f8fdebccef99aaf5300da9b5
gopherstein <spankenstien@gmail.com>
2016-05-06 15:27:43 -0500

    version bump

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version CLI release version.
-const Version string = "0.5.9"
+const Version string = "0.5.10"
 
 // APIVersion Currently certified API Version.
 const APIVersion = "v0.5.11"
